7e97c96316a36e894c5415b56f3db4be4b7b0176
Ariel <mileguir23@hotmail.com>
2020-11-09 20:11:51 -0300

    Voltando ao normal

--- Modulos/Banco_de_Dados.py
+++ Modulos/Banco_de_Dados.py
@@ -1,3 +1,9 @@
+import mysql.connector as mysql
+
+def Conecta_Banco()
+    db = mysql.connect(host = "localhost", user = "ariel", passwd = "123456789")
+    return db
+
 def Salva_jogador():
     """Salva o nome do usuário que foi cadastrado"""
     return 0
@@ -5,4 +11,3 @@ def Salva_jogador():
 def Salva_partida():
     """Salva o histórico de partidas"""
     return 0
-    
--- README.txt
+++ README.txt
@@ -4,5 +4,3 @@ trabalho de python
 #Todos os casos de testes se encontram no arquivo .py Testa_Tudo
 #Os arquivos de testes como Testa_Peao, Testa_Dado, etc, se referem aos testes relativos aqueles módulos.
 #Atenção ao caso de teste do módulo Player
-
-olá
